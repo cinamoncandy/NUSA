9aed79dae82b9136f24a97c2727d7d6799c02838
cinamoncandy <DESPORIN12@GMAIL.COM>
2026-07-16 22:05:35 +0900

    Export order operational restrictions

--- apps/execution/src/index.ts
+++ apps/execution/src/index.ts
@@ -2,4 +2,5 @@ export * from "./pre-trade-risk";
 export * from "./order-admission";
 export * from "./execution-gateway";
 export * from "./order-reconciliation";
+export * from "./order-restriction";
 export * from "./final-certification";
