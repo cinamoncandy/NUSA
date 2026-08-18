523896a1afdaeae9e235b3fcfaa290d0d0e1b672
cinamoncandy <DESPORIN12@GMAIL.COM>
2026-08-18 22:15:53 +0900

    test(e2e): serialize Playwright workers in CI

--- playwright.config.js
+++ playwright.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("@playwright/test");
 
 module.exports = defineConfig({
   testDir: "./tests/e2e",
+  workers: process.env.CI ? 1 : undefined,
   use: { baseURL: "http://127.0.0.1:4173", headless: true },
   webServer: {
     command: "node tests/e2e/serve-components.js",
