c077afadba1eb56188b23272d2ad16e875dd4fb2
cinamoncandy <DESPORIN12@GMAIL.COM>
2026-09-03 14:32:53 +0900

    feat(ui): activate selected Android concept D

--- apps/mobile/src/homeView.tsx
+++ apps/mobile/src/homeView.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Platform } from "react-native";
-import { AndroidCHomeView } from "./androidCHomeView";
+import { AndroidDHomeView } from "./androidDHomeView";
 import { HomeView as LegacyHomeView } from "./homeViewLegacy";
 
 export type { HomeDestination } from "./homeViewLegacy";
 
 export function HomeView(props: React.ComponentProps<typeof LegacyHomeView>) {
   return Platform.OS === "android"
-    ? <AndroidCHomeView {...props} />
+    ? <AndroidDHomeView {...props} />
     : <LegacyHomeView {...props} />;
 }
