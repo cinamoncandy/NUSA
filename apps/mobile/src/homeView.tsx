3b47451ba300e18c9e66f86d6920d387890ec997
cinamoncandy <DESPORIN12@GMAIL.COM>
2026-09-03 12:09:57 +0900

    refactor(android-ui): route selected concept A

--- apps/mobile/src/homeView.tsx
+++ apps/mobile/src/homeView.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Platform } from "react-native";
-import { AndroidEHomeView } from "./androidEHomeView";
+import { AndroidAHomeView } from "./androidAHomeView";
 import { HomeView as LegacyHomeView } from "./homeViewLegacy";
 
 export type { HomeDestination } from "./homeViewLegacy";
 
 export function HomeView(props: React.ComponentProps<typeof LegacyHomeView>) {
   return Platform.OS === "android"
-    ? <AndroidEHomeView {...props} />
+    ? <AndroidAHomeView {...props} />
     : <LegacyHomeView {...props} />;
 }
