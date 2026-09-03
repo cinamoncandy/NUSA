683d38ad2956fcffc74cc50a0acec5d22e8ce2b7
cinamoncandy <DESPORIN12@GMAIL.COM>
2026-09-03 12:19:05 +0900

    feat(ui): route Android Home to concept B

--- apps/mobile/src/homeView.tsx
+++ apps/mobile/src/homeView.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Platform } from "react-native";
-import { AndroidAHomeView } from "./androidAHomeView";
+import { AndroidBHomeView } from "./androidBHomeView";
 import { HomeView as LegacyHomeView } from "./homeViewLegacy";
 
 export type { HomeDestination } from "./homeViewLegacy";
 
 export function HomeView(props: React.ComponentProps<typeof LegacyHomeView>) {
   return Platform.OS === "android"
-    ? <AndroidAHomeView {...props} />
+    ? <AndroidBHomeView {...props} />
     : <LegacyHomeView {...props} />;
 }
