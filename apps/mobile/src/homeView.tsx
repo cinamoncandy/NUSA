16183a920a3ab411ebe13fa0845a9c37de7ce97d
cinamoncandy <DESPORIN12@GMAIL.COM>
2026-09-03 11:46:04 +0900

    feat(android-ui): route Android home to concept E

--- apps/mobile/src/homeView.tsx
+++ apps/mobile/src/homeView.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Platform } from "react-native";
-import { AndroidNowView } from "./androidNowView";
+import { AndroidEHomeView } from "./androidEHomeView";
 import { HomeView as LegacyHomeView } from "./homeViewLegacy";
 
 export type { HomeDestination } from "./homeViewLegacy";
 
 export function HomeView(props: React.ComponentProps<typeof LegacyHomeView>) {
   return Platform.OS === "android"
-    ? <AndroidNowView {...props} />
+    ? <AndroidEHomeView {...props} />
     : <LegacyHomeView {...props} />;
 }
